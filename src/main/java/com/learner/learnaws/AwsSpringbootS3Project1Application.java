14c8bcd989bdb99703e50da642dfe6c10ec4900f
anupam <anupam.nitdgp14@gmail.com>
2025-05-03 19:18:32 +0530

    Initial Commit - Initialize the project.

---
+++ src/main/java/com/learner/learnaws/AwsSpringbootS3Project1Application.java
@@ -0,0 +1,13 @@
+package com.learner.learnaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootS3Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringbootS3Project1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learner/learnaws/AwsSpringbootS3Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.learnaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootS3Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
